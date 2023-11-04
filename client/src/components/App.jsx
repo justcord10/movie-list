6f537cb6244a0e769382545cde49a2d8218528bc
Justin Cordova <justcord10@gmail.com>
2023-11-04 15:21:22 -0700

    cleared the changes made in index.js for better separation of concerns and moved the api request into app.jsx. also added in logic to send proper API requests to update the databasae when changes to movies are made.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,103 +7,99 @@ import MovieListItem from './MovieListItem.jsx';
 import Search from './Search.jsx';
 import Add from './Add.jsx';
 
+
 const {useState} = React;
 
-//need to change to allow js logic
-const App = ({movies}) => {
-  console.log(movies);
+const App = () => {
+
 //States
-  //input text down in Search.jsx
-  //movie list
-  const [movieList, setMovieList] = React.useState(movies);
-  //search Results List
-  const [searchResultsList, setSearchResultsList] = React.useState('');// which one is clearer??? this or const [query, setQuery] = useState('');
-  //displayed List
+  const [movieList, setMovieList] = React.useState([]);
   const [displayedList, setDisplayedList] = React.useState([]);
 
+  //use useEffect with an empty array in second arg to only do something once on first render.
+  React.useEffect( () => {
+    //apiCall
+    axios.get('/api/movies')
+    .then(function (res) {
+      const movies = res.data;
+      //set movie list and displayed list
+      setMovieList(movies);
+      setDisplayedList(movies);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert(err);
+    });
+  }, []);
 
   //event Handler functions
   const searchHandleClick = (query) => {
-    console.log('Clicked!');
-    console.log(query);
-    //declare empty array for search results list variable
     var results = [];
-    //if query is '' then
     if (query === '') {
-      //set display list to be the movie list
       setDisplayedList(movieList);
     }
-    //store a deep copy of the movie list array
     var copyList = JSON.parse(JSON.stringify(movieList));
-    //iterate through the copylist
     for (let i = 0; i < copyList.length; i++) {
-      //if the current objects value string contains the query then
       if (copyList[i].title.includes(query)) {
-        //push it onto the results array
         results.push(copyList[i]);
       }
     }
-    //if out results list is empty then...
     if (results.length === 0) {
-      //set the search results list with [{title:'no movie by that name found'}]
       setDisplayedList([{title:'no movie by that name found'}]);
     } else {
-      //set the search results list with the results array
       setDisplayedList(results);
     }
-    //debugger;
     console.log(displayedList);
   }
 
   const addHandleClick = (movieTitle) => {
-    //console.log('clicked');
-    //console.log(movieTitle);
-    //new variable to hold the clone array of objects, with the added movie's object (use the spread operator!!)
     var newMovieList = [...movieList, {title:movieTitle, watched: false}];
-    console.log(newMovieList);
-    //set the movie list with the new one
     setMovieList(newMovieList);
-    //set the displayed movie list with the new one
     setDisplayedList(newMovieList);
+    //need to update the db with an api call
+    axios.post('/api/movies', {name:movieTitle})
+    .then(function (res) {
+      const movies = res.data;
+      console.log()
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert(err);
+    });
   };
 
-  //watched click handler
   const watchedHandleClick = (title) => {
-    //copy movie List
     var copyList = [...movieList];
-    //iterate through the list
     for (let i = 0; i < copyList.length; i++) {
-      //toggle current elements watched property when the title is found
       if (copyList[i].title === title) {
         copyList[i].watched === false ? copyList[i].watched = true : copyList[i].watched = false;
       };
     }
-    //set movie list and displayed movie list
-    console.log(copyList); //toggles properly!
     setMovieList(copyList);
     setDisplayedList(copyList);
+    //toggle the db resource watched property for the movie with the name === title
+    axios.put('/api/movies', {name:title})
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert(err);
+    });
   };
 
   //new filter click handler
   const filterHandleClick = (isWatched) => {
     console.log(isWatched);
-    //declare empty results array
     var results = [];
-    //copy movieList using spread operator
     var copyList = [...movieList];
-    //iterate through the copy list
     for (let i = 0; i < copyList.length; i++) {
-      //if the current movies watched value matches isWatched
-      //push that object onto the results list
       copyList[i].watched === isWatched ? results.push(copyList[i]): results;
     }
-    //test passed //console.log(results);
-    //set the displayedList with results
     setDisplayedList(results);
   };
 
-  //need to have a title at the top in its own little bar
-  //need to render out the movie list component in its own div with movies passed in
+
   return (
     <div>
       <h1 className="title">MovieList</h1>
@@ -118,6 +114,4 @@ const App = ({movies}) => {
   );
 }
 
-//ACTUALLY!! dont need to do it is done in the index.js file!!
-//render the page for the first time
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-var axios = require('axios')
-axios.get('/api/movies')
-  .then(function (res) {
-    const movies = res.body;
-    ReactDOM.render(<App movies ={movies}/>, document.getElementById('app'));
-  })
-  .catch(function (err) {
-    console.log(err)
-  })
+
 //make a axios get request and then render the DOM as a callback with the props passed in
-//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
